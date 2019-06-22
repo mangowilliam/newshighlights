b45380583ef80556fb4569635f5fbab165418179
williammango <williammango2015@gmail.com>
2019-06-22 11:27:47 +0300

    create configuration class and subclasses

--- app/views.py
+++ app/views.py
@@ -1,10 +1,21 @@
 from flask import render_template
+from app import app
 
-
+#views
 # Views
 @app.route('/')
 def index():
+
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title = "Home - All news around the globe"
+    return render_template('index.html', title = title)
+
+@app.route('/source/<source_id>')
+def source(source_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('source.html',id = source_id)
